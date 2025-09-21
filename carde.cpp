b84a1a7e3496b650e9f965fe36b16d5dee8aa3cd
Krushna Waman <120102116+Krishnawaman25@users.noreply.github.com>
2025-09-21 14:59:11 +0530

    Add files via upload

---
+++ carde.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class cars{
+	private :
+		string make;
+		string model;
+		int year;
+	public :
+		cars(string make,string model,int year){
+			this->make=make;
+			this->model=model;
+			this->year=year;
+		cout<<"car created:"<<make	<<" "<<model<<"("<<year<<")"<<endl;
+		}
+	~cars(){
+		cout<<"Destroying car"<<make <<" "<<model<<"("<<year<<")"<<endl;
+	}
+	void display(){
+		cout<<"Make: "<<make<<",Model"<<model<<",year"<<year;
+	}
+	
+};
+int main(){
+	cars car1("Toyota","Corolla",1960);
+	cars car2("Honda","Civic",2022);
+	
+	cout<<"\ncar Details:\n";
+	car1.display();
+	car2.display();
+}
